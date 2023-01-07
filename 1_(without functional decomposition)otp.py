7fc5f829066bcabef538b6931f3e136b4393cb1e
vishalbhure611 <109169840+vishalbhure611@users.noreply.github.com>
2023-01-07 09:50:01 +0530

    Create 1_(without functional decomposition)otp.py

--- 1_(without functional decomposition)otp.py
+++ 1_(without functional decomposition)otp.py
@@ -1,24 +1,17 @@
-import random #random module to get random integers to create OTP
-import senders_data1 #email and password of sender from another file
-import smtplib #simple message transfer protocol#library to send email to users email address
-
-n=6
-OTP=""
-for i in range(n):
-    OTP+=str(random.randint(0,9))
-
-server =smtplib.SMTP('smtp.gmail.com',587)
-Senders_email = senders_data1.email
-Senders_password= senders_data1.password
-
-server.starttls()
-server.login(Senders_email, password=Senders_password) 
-
-receivers_name=input("Enter receivers name ")
-receivers_email=input("Enter receivers email ")
-
-msg=("Hi "+ receivers_name +"\n"+ str(OTP)+" is your OTP ")
-print (msg)
-server.sendmail (Senders_email, receivers_email,msg)
-server.quit() 
-print("email has been sent!")
+import random 
+import smtplib # send mail from sender to user
+server = smtplib.SMTP('smtp.gmail.com',587)
+server .starttls()
+server.login('vishalbhure611@gmail.com',password='gnkr aozb cugp zxsa')
+otp = ''.join ([str(random.randint(0,9))for i in range(4)])
+msg = 'hello,your otp is '+str(otp)
+vr = msg
+rc = str(input("Enter receiver's mail: "))
+server.sendmail('abhijeetbhingole671@gmail.com',str(rc),msg)
+server.quit()
+print(otp)
+temp = str(input("Enter received OTP: "))
+if(otp==temp):
+    print("Verified..!!")
+else:
+    print(" not verified")
